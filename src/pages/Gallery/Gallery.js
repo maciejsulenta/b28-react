7296994b07222ffab9470ea6fd360e141c60077c
maciejsulenta <maciej.sulenta@gmail.com>
2022-06-09 14:09:56 +0200

    gallery : fix: code cleanup

--- src/pages/Gallery/Gallery.js
+++ src/pages/Gallery/Gallery.js
@@ -6,7 +6,7 @@ export const Container = styled.div`
   width: 500vw;
 `;
 export const Slide = styled.div`
-  width: 100%;
+  flex: 1;
   height: 100vh;
   background-color: ${(props) => (props.bgc ? "brown" : "cadetblue")};
 `;
--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,18 +1,16 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
-import { useEffect, useRef } from "react";
+import { useEffect } from "react";
 import { Container, Slide } from "./Gallery.js";
 const Gallery = () => {
   gsap.registerPlugin(ScrollTrigger);
 
-  const ref = useRef();
-  const refs = useRef([]);
   useEffect(() => {
     const components = document.querySelectorAll("#component");
     const container = document.querySelector("#container");
 
     gsap.to(components, {
-      xPercent: -100 - (refs.current.length - 1),
+      xPercent: -100 - (components.length - 1),
       ease: "none",
       scrollTrigger: {
         trigger: container,
