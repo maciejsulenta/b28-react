28c855d3e6516f979ff3984a0d473547174e2111
maciejsulenta <maciej.sulenta@gmail.com>
2022-06-09 16:45:29 +0200

    gallery : feature: structure

--- src/pages/Gallery/index.jsx
+++ src/pages/Gallery/index.jsx
@@ -1,7 +1,13 @@
 import gsap from "gsap";
 import ScrollTrigger from "gsap/dist/ScrollTrigger";
 import { useEffect } from "react";
-import { Container, Slide } from "./Gallery.js";
+import Gallery1 from "../../assets/images/gallery1.png";
+import Gallery2 from "../../assets/images/gallery2.png";
+import Gallery3 from "../../assets/images/gallery3.png";
+import Gallery4 from "../../assets/images/gallery4.png";
+import Gallery5 from "../../assets/images/gallery5.png";
+import Gallery6 from "../../assets/images/gallery6.png";
+import { GalleryWrap, Slide, ImageContainer, Title } from "./Gallery.js";
 const Gallery = () => {
   gsap.registerPlugin(ScrollTrigger);
 
@@ -10,7 +16,7 @@ const Gallery = () => {
     const container = document.querySelector("#container");
 
     gsap.to(components, {
-      xPercent: -100 - (components.length - 1),
+      xPercent: -100 * (components.length - 1),
       ease: "none",
       scrollTrigger: {
         trigger: container,
@@ -24,12 +30,49 @@ const Gallery = () => {
 
   return (
     <>
-      <Container id="container">
+      <GalleryWrap id="container">
         <Slide bgc id="component">
-          Primeiro
+          <ImageContainer col={"2/4"} row={"1/9"} img={Gallery1} />
+          <ImageContainer col={"13/15"} row={"2/8"} img={Gallery2} />
+          <ImageContainer col={"3/6"} row={"11/19"} img={Gallery3} />
+          <ImageContainer col={"8/11"} row={"5/15"} img={Gallery4} />
+          <ImageContainer col={"15/18"} row={"13/21"} img={Gallery5} />
+          <ImageContainer col={"17/20"} row={"4/11"} img={Gallery6} />
         </Slide>
-        <Slide id="component">sikutko</Slide>
-      </Container>
+        <Slide bgc id="component">
+          <ImageContainer col={"2/4"} row={"1/9"} img={Gallery1} />
+          <ImageContainer col={"13/15"} row={"2/8"} img={Gallery2} />
+          <ImageContainer col={"3/6"} row={"11/19"} img={Gallery3} />
+          <ImageContainer col={"8/11"} row={"5/15"} img={Gallery4} />
+          <ImageContainer col={"15/18"} row={"13/21"} img={Gallery5} />
+          <ImageContainer col={"17/20"} row={"4/11"} img={Gallery6} />
+        </Slide>
+        <Slide bgc id="component">
+          <ImageContainer col={"2/4"} row={"1/9"} img={Gallery1} />
+          <ImageContainer col={"13/15"} row={"2/8"} img={Gallery2} />
+          <ImageContainer col={"3/6"} row={"11/19"} img={Gallery3} />
+          <ImageContainer col={"8/11"} row={"5/15"} img={Gallery4} />
+          <ImageContainer col={"15/18"} row={"13/21"} img={Gallery5} />
+          <ImageContainer col={"17/20"} row={"4/11"} img={Gallery6} />
+        </Slide>
+        <Slide bgc id="component">
+          <ImageContainer col={"2/4"} row={"1/9"} img={Gallery1} />
+          <ImageContainer col={"13/15"} row={"2/8"} img={Gallery2} />
+          <ImageContainer col={"3/6"} row={"11/19"} img={Gallery3} />
+          <ImageContainer col={"8/11"} row={"5/15"} img={Gallery4} />
+          <ImageContainer col={"15/18"} row={"13/21"} img={Gallery5} />
+          <ImageContainer col={"17/20"} row={"4/11"} img={Gallery6} />
+        </Slide>
+        <Slide bgc id="component">
+          <ImageContainer col={"2/4"} row={"1/9"} img={Gallery1} />
+          <ImageContainer col={"13/15"} row={"2/8"} img={Gallery2} />
+          <ImageContainer col={"3/6"} row={"11/19"} img={Gallery3} />
+          <ImageContainer col={"8/11"} row={"5/15"} img={Gallery4} />
+          <ImageContainer col={"15/18"} row={"13/21"} img={Gallery5} />
+          <ImageContainer col={"17/20"} row={"4/11"} img={Gallery6} />
+        </Slide>
+        <Title>Nasze prace</Title>
+      </GalleryWrap>
     </>
   );
 };
