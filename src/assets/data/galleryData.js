987cbd8b2b8fc7433f27ccd670c161037f129a5b
maciejsulenta <maciej.sulenta@gmail.com>
2022-06-10 00:05:33 +0200

    gallery : fix: galleryData images update

--- src/assets/data/galleryData.js
+++ src/assets/data/galleryData.js
@@ -4,35 +4,46 @@ import Gallery3 from "../../assets/images/gallery3.png";
 import Gallery4 from "../../assets/images/gallery4.png";
 import Gallery5 from "../../assets/images/gallery5.png";
 import Gallery6 from "../../assets/images/gallery6.png";
+import Gallery7 from "../../assets/images/gallery7.png";
+import Gallery8 from "../../assets/images/gallery8.png";
+import Gallery9 from "../../assets/images/gallery9.png";
+import Gallery10 from "../../assets/images/gallery10.png";
+import Gallery11 from "../../assets/images/gallery11.png";
+import Gallery12 from "../../assets/images/gallery12.png";
+import Gallery13 from "../../assets/images/gallery13.png";
+import Gallery14 from "../../assets/images/gallery14.png";
+import Gallery15 from "../../assets/images/gallery15.png";
+import Gallery16 from "../../assets/images/gallery16.png";
+import Gallery17 from "../../assets/images/gallery17.png";
+import Gallery18 from "../../assets/images/gallery18.png";
+import Gallery19 from "../../assets/images/gallery19.png";
+import Gallery20 from "../../assets/images/gallery20.png";
+import Gallery21 from "../../assets/images/gallery21.png";
+import Gallery22 from "../../assets/images/gallery22.png";
+import Gallery23 from "../../assets/images/gallery23.png";
 
 export const galleryData = [
   { id: 1, col: "2/4", row: "1/9", img: Gallery1 },
   { id: 2, col: "14/16", row: "1/8", img: Gallery2 },
-  { id: 3, col: "3/6", row: "11/19", img: Gallery3 },
-  { id: 4, col: "8/11", row: "5/15", img: Gallery4 },
+  { id: 3, col: "4/7", row: "11/20", img: Gallery3 },
+  { id: 4, col: "9/12", row: "4/14", img: Gallery4 },
   { id: 5, col: "15/18", row: "13/21", img: Gallery5 },
-  { id: 6, col: "19/22", row: "4/11", img: Gallery6 },
-
-  { id: 7, col: "24/27", row: "1/12", img: Gallery1 },
-  { id: 8, col: "26/28", row: "14/20", img: Gallery2 },
-  { id: 9, col: "30/32", row: "3/11", img: Gallery3 },
-  { id: 10, col: "35/40", row: "4/18", img: Gallery4 },
-  //od
-  { id: 11, col: "44/47", row: "9/20", img: Gallery5 },
-  { id: 12, col: "49/52", row: "14/21", img: Gallery6 },
-
-  { id: 13, col: "54/57", row: "1/8", img: Gallery1 },
-  { id: 14, col: "59/62", row: "2/13", img: Gallery2 },
-  //do
-  { id: 15, col: "68/70", row: "2/9", img: Gallery3 },
-  { id: 16, col: "68/70", row: "13/20", img: Gallery4 },
-  { id: 17, col: "71/73", row: "2/9", img: Gallery5 },
-  { id: 18, col: "71/73", row: "13/20", img: Gallery6 },
-
-  { id: 19, col: "74/76", row: "2/9", img: Gallery1 },
-  { id: 20, col: "74/76", row: "13/20", img: Gallery2 },
-  { id: 21, col: "80/84", row: "4/17", img: Gallery3 },
-  { id: 22, col: "89/91", row: "13/21", img: Gallery4 },
-  { id: 23, col: "93/95", row: "2/8", img: Gallery5 },
-  { id: 24, col: "97/100", row: "7/15", img: Gallery6 },
+  { id: 6, col: "19/22", row: "3/11", img: Gallery6 },
+  { id: 7, col: "24/27", row: "1/12", img: Gallery7 },
+  { id: 8, col: "26/29", row: "14/20", img: Gallery8 },
+  { id: 9, col: "30/33", row: "3/9", img: Gallery9 },
+  { id: 10, col: "35/42", row: "4/18", img: Gallery10 },
+  { id: 11, col: "46/50", row: "11/21", img: Gallery11 },
+  { id: 12, col: "51/54", row: "1/8", img: Gallery12 },
+  { id: 13, col: "56/61", row: "5/18", img: Gallery13 },
+  { id: 14, col: "65/67", row: "2/9", img: Gallery14 },
+  { id: 15, col: "65/67", row: "13/20", img: Gallery15 },
+  { id: 16, col: "68/70", row: "2/9", img: Gallery16 },
+  { id: 17, col: "68/70", row: "13/20", img: Gallery17 },
+  { id: 18, col: "71/73", row: "2/9", img: Gallery18 },
+  { id: 19, col: "71/73", row: "13/20", img: Gallery19 },
+  { id: 20, col: "78/85", row: "4/18", img: Gallery20 },
+  { id: 21, col: "88/91", row: "12/21", img: Gallery21 },
+  { id: 22, col: "92/95", row: "2/10", img: Gallery22 },
+  { id: 23, col: "97/100", row: "7/15", img: Gallery23 },
 ];
